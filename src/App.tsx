404f0a3999ff4de2de3d7853b02c7cf3b9bc2f33
HebaHamdan2 <hebashamdan2@gmail.com>
2025-03-17 01:14:47 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout/Layout'
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        {/* <Route path='/home' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} /> */}
+      </Route>
+    )
+  )
+  return (
+    <>
+       <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <div className="wrapper mt-10 mb-4">
+          <div>
+          <img src="../../../src/assets/Exclusive.svg" alt="logo" />
+          </div>
+          <div>
+            <ul>
+                <li>Signup</li>
+                <li></li>
+                <li></li>
+            </ul>
+          </div>
+             
+        </div>
+    );
+}
+
+export default Navbar;
