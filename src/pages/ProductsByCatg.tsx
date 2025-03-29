0cea9b0295fa6cc76cb934712cc94d92f9cc882b
HebaHamdan2 <hebashamdan2@gmail.com>
2025-03-29 03:03:45 +0300

    categories slider done

--- src/pages/ProductsByCatg.tsx
+++ src/pages/ProductsByCatg.tsx
@@ -1,6 +1,6 @@
 import CategoryProducts from '../components/ProductsOfCategory/CategoryProducts';
 
-const Products = () => {
+const ProductsByCatg= () => {
     return (
         <div>
         <CategoryProducts/>
@@ -8,4 +8,4 @@ const Products = () => {
     );
 }
 
-export default Products;
+export default ProductsByCatg;
